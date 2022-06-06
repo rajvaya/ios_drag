1ffe92ad78e2af53087f8e44e4ac43635e63a698
raj vaya <rajkumarvaya@gmail.com>
2022-06-06 19:40:07 +0530

    Initial Commit

---
+++ DummyDrawer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DummyDrawer
+//
+//  Created by raj vaya on 06/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DummyDrawer/DummyDrawerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DummyDrawerApp.swift
+//  DummyDrawer
+//
+//  Created by raj vaya on 06/06/22.
+//
+
+import SwiftUI
+
+@main
+struct DummyDrawerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
